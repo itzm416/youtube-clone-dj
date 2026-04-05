005531901af76b6f3e49d7d1a90dc822b4b26233
itzm416 <itzrahul6449@gmail.com>
2026-04-06 01:42:24 +0530

    test deployment

--- pro/settings.py
+++ pro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3v7s#a)nlv+d1bobp6d5@j5_l(c(1!_8qnxd!spybb&%_6m9a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["13.221.59.111"]
 
